28b54fbeac4aa2ce96119a627849d6a516fb67e9
AoyuQC <aoyuzhang1989@gmail.com>
2024-05-15 05:34:07 +0000

    add half body script

---
+++ run/half_body_inference.sh
@@ -0,0 +1,4 @@
+python /home/ubuntu/pytorch_gpu_base_ubuntu_uw2_workplace/aws-gcr-csdc-atl/aigc-vto-models/aigc-vto-models-ootd/reference/OOTDiffusion/run/run_ootd.py --model_path  /home/ubuntu/pytorch_gpu_base_ubuntu_uw2_workplace/aws-gcr-csdc-atl/aigc-vto-models/aigc-vto-models-ootd/reference/OOTDiffusion/run/examples/model/01008_00.jpg \
+--cloth_path  /home/ubuntu/pytorch_gpu_base_ubuntu_uw2_workplace/aws-gcr-csdc-atl/aigc-vto-models/aigc-vto-models-ootd/reference/OOTDiffusion/run/examples/garment/00055_00.jpg \
+--scale 2.0 \
+--sample 4
--- run/run_ootd.py
+++ run/run_ootd.py
@@ -11,13 +11,13 @@ from preprocess.humanparsing.run_parsing import Parsing
 from ootd.inference_ootd_hd import OOTDiffusionHD
 from ootd.inference_ootd_dc import OOTDiffusionDC
 
-import debugpy
-
-debugpy.listen(5889)  # 5678 is port
-print("Waiting for debugger attach")
-debugpy.wait_for_client()
-debugpy.breakpoint()
-print('break on this line')
+#import debugpy
+#
+#debugpy.listen(5889)  # 5678 is port
+#print("Waiting for debugger attach")
+#debugpy.wait_for_client()
+#debugpy.breakpoint()
+#print('break on this line')
 
 output_path = "/home/ubuntu/pytorch_gpu_base_ubuntu_uw2_workplace/aws-gcr-csdc-atl/aigc-vto-models/aigc-vto-models-ootd/reference/OOTDiffusion/run"
 
