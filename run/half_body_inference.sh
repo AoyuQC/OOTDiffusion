c0378a350effca3381cc550885e327a5463c37c5
AoyuQC <aoyuzhang1989@gmail.com>
2024-05-18 02:28:56 +0000

    fix typo

--- run/half_body_inference.sh
+++ run/half_body_inference.sh
@@ -6,14 +6,14 @@ echo "GPU name: $gpu_info"
 if [[ "$gpu_info" == *"A100"* ]]; then
     echo "The GPU is an A100 GPU."
     python /home/ec2-user/SageMaker/vto/OOTDiffusion/run/run_ootd.py --model_path /home/ec2-user/SageMaker/vto/OOTDiffusion/run/examples/garment/00055_00.jpg \
-        --cloth_path /home/ec2-user/SageMaker/vto/OOTDiffusion/run/examples/model/i01008_00.jpg \
+        --cloth_path /home/ec2-user/SageMaker/vto/OOTDiffusion/run/examples/model/01008_00.jpg \
         --scale 2.0 \
         --checkpoint_id checkpoint-36000 \
         --sample 1
 elif [[ "$gpu_info" == *"A10"* ]]; then
     echo "The GPU is an A10 GPU."
     python /home/ubuntu/pytorch_gpu_base_ubuntu_uw2_workplace/aws-gcr-csdc-atl/aigc-vto-models/aigc-vto-models-ootd/reference/OOTDiffusion/run/run_ootd.py --model_path /home/ubuntu/pytorch_gpu_base_ubuntu_uw2_workplace/aws-gcr-csdc-atl/aigc-vto-models/aigc-vto-models-ootd/reference/OOTDiffusion/run/examples/garment/00055_00.jpg \
-        --cloth_path /home/ubuntu/pytorch_gpu_base_ubuntu_uw2_workplace/aws-gcr-csdc-atl/aigc-vto-models/aigc-vto-models-ootd/reference/OOTDiffusion/run/examples/model/i01008_00.jpg \
+        --cloth_path /home/ubuntu/pytorch_gpu_base_ubuntu_uw2_workplace/aws-gcr-csdc-atl/aigc-vto-models/aigc-vto-models-ootd/reference/OOTDiffusion/run/examples/model/01008_00.jpg \
         --scale 2.0 \
         --checkpoint_id checkpoint-epoch0 \
         --sample 1
