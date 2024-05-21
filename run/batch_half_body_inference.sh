5cf42a41b46e954b24fa28a9e48a1bd35a1b6482
AoyuQC <aoyuzhang1989@gmail.com>
2024-05-21 08:56:50 +0000

    update batch to support args

--- run/batch_half_body_inference.sh
+++ run/batch_half_body_inference.sh
@@ -1,22 +1,25 @@
 #!/bin/bash
 gpu_info=$(nvidia-smi --query-gpu=gpu_name --format=csv,noheader)
 
-echo "GPU name: $gpu_info"
+txt_file="$1"
+checkpoint_id="$2"
+gpu_id="$3"
 
 if [[ "$gpu_info" == *"A100"* ]]; then
     echo "The GPU is an A100 GPU."
     python /home/ec2-user/SageMaker/vto/OOTDiffusion/run/run_ootd_batch.py --base_path /home/ec2-user/SageMaker/data/dataset/vto/shenin/shein_data \
-        --txt_file /home/ec2-user/SageMaker/data/dataset/vto/shenin/remaining_test_pairs_shein.txt \
+        --txt_file $txt_file \
         --scale 4.0 \
-        --checkpoint_id checkpoint-36000 \
-	    --gpu_id 1 \
+        --checkpoint_id $checkpoint_id \
+        --gpu_id $gpu_id \
         --sample 1
 elif [[ "$gpu_info" == *"A10"* ]]; then
     echo "The GPU is an A10 GPU."
-    python /home/ubuntu/pytorch_gpu_base_ubuntu_uw2_workplace/aws-gcr-csdc-atl/aigc-vto-models/aigc-vto-models-ootd/reference/OOTDiffusion/run/run_ootd_batch.py --base_path /home/ubuntu/dataset/aigc-app-vto/shenin/shein_data \
-        --txt_file /home/ubuntu/dataset/aigc-app-vto/shenin/test_pairs_shein.txt \
+    python /home/ubuntu/pytorch_gpu_base_ubuntu_uw2_workplace/aws-gcr-csdc-atl/aigc-vto-models/aigc-vto-models-ootd/reference/OOTDiffusion/run/run_ootd_batch.py --base_path $base_path \
+        --txt_file $txt_file \
         --scale 2.0 \
-        --checkpoint_id checkpoint-epoch0 \
+        --checkpoint_id $checkpoint_id \
+        --gpu_id $gpu_id \
         --sample 1
 else
     echo "The GPU is neither A10 nor A100."
